9e8175267e9e1233938d98256b902aa475e5f445
Jessica Zager <jmzager@gmail.com>
2022-08-20 14:44:36 -0500

    Only stashed stuff I want to keep

--- app-og.js
+++ app-og.js
@@ -14,3 +14,19 @@ window.addEventListener("load", function(){
         });
     })
 })
+let highest = 0;
+let highest_day;
+let days = document.getElementsByClassName('day');
+let amounts = document.getElementsByClassName('amount');
+for (let i=0; i<numbers.length; i++){
+  if (numbers[i].amount > highest){
+    highest = numbers[i].amount;
+    highest_day = numbers[i].day;
+    let height = (numbers[i].amount/highest)*75 + '%';
+    document.getElementsByClassName('amount')[i].getElementsByClassName.height = height;
+    if (days[i].innerHTML == highest_day){
+      days[i].previousElementSibling.style.background = 'hsl(186, 34%, 60%)';
+      
+    }
+  }
+}
